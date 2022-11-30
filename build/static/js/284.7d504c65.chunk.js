b1909b5be045b434b729d535cc0062394142693e
sveltose123 <priyajaindevelopersveltose013@gmail.com>
2022-11-30 12:39:22 +0530

    Update

--- build/static/js/284.7d504c65.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[284],{2198:function(a,s){}}]);
-//# sourceMappingURL=284.7d504c65.chunk.js.map
