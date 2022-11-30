00402b7d4bc6305aadae4a4018eac9bb98429221
sveltose123 <priyajaindevelopersveltose013@gmail.com>
2022-11-30 12:49:13 +0530

    nobuildd

--- build/static/js/1.5db03dbc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[1],{826:function(a,s,d){}}]);
-//# sourceMappingURL=1.5db03dbc.chunk.js.map
--- build/static/js/294.081cd13f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[294],{2215:function(a,s){}}]);
-//# sourceMappingURL=294.081cd13f.chunk.js.map
